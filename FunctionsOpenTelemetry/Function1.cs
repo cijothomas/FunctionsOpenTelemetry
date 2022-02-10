3564f3579956dc31b0c7533d971ca0c8a5883847
Cijo Thomas <cithomas@microsoft.com>
2022-02-10 12:46:27 -0800

    add logs metrics and traces

--- FunctionsOpenTelemetry/Function1.cs
+++ FunctionsOpenTelemetry/Function1.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace FunctionsOpenTelemetry
 {
@@ -17,7 +18,7 @@ namespace FunctionsOpenTelemetry
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogWarning("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
@@ -28,6 +29,9 @@ namespace FunctionsOpenTelemetry
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            log.LogWarning("Name is {name}", name);
+            Activity.Current?.SetTag("name", name);
 
             return new OkObjectResult(responseMessage);
         }
---
+++ FunctionsOpenTelemetry/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+[assembly: FunctionsStartup(typeof(FunctionsOpenTelemetry.Startup))]
+namespace FunctionsOpenTelemetry
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // OpenTelemetry Resource to be associated with logs, metrics and traces
+            var openTelemetryResourceBuilder = ResourceBuilder.CreateDefault().AddService("opentelemetry-service");
+
+            // Enable Logging with OpenTelemetry
+            builder.Services.AddLogging( (loggingBuilder) => 
+                {
+                    // Only Warning or above will be sent to Opentelemetry
+                    loggingBuilder.AddFilter<OpenTelemetryLoggerProvider>("*", LogLevel.Warning);
+                }
+            );
+            
+            builder.Services.AddSingleton<ILoggerProvider, OpenTelemetryLoggerProvider>();
+            builder.Services.Configure<OpenTelemetryLoggerOptions>( (openTelemetryLoggerOptions) =>
+                {
+                    openTelemetryLoggerOptions.SetResourceBuilder(openTelemetryResourceBuilder);
+                    openTelemetryLoggerOptions.IncludeFormattedMessage = true;
+                    openTelemetryLoggerOptions.AddConsoleExporter();
+                }
+            );
+
+            // Enable Tracing with OpenTelemetry
+            var openTelemetryTracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetResourceBuilder(openTelemetryResourceBuilder)
+                .SetSampler(new AlwaysOnSampler())
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter()
+                .Build();
+            builder.Services.AddSingleton(openTelemetryTracerProvider);
+
+            // Enable Metrics with OpenTelemetry
+            var openTelemetryMeterProvider = Sdk.CreateMeterProviderBuilder()
+                .SetResourceBuilder(openTelemetryResourceBuilder)
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter(consoleOptions =>
+                    {
+                        consoleOptions.MetricReaderType = MetricReaderType.Periodic;
+                        consoleOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 10000;
+                    })
+                .Build();
+            builder.Services.AddSingleton(openTelemetryMeterProvider);
+        }
+    }
+}
